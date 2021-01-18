cd571ce303c49a72b1da4b1913b7d0a5f1fc6251
Mike Ackison <mike.ackison@shopify.com>
2021-01-18 20:34:27 +0000

    add route for urls to pass date to template urls_index

--- express_server.js
+++ express_server.js
@@ -24,6 +24,13 @@ app.get('/hello' ,(req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+// test to illustrate scoping
 // app.get('/set', (req, res) => {
 //   const a = 1;
 //   res.send(`a = ${a}`);
