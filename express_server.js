8ab04ae0273bfea269c2eb8fb730d0c9c88ddda6
Mike Ackison <mike.ackison@shopify.com>
2021-01-18 18:20:16 +0000

    First commit

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 8080; //default port *be sure old test is not running anymore!
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+// registers a handler on the root path, "/".
+app.get('/', (req, res) => {
+res.send('Hello There!');
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+
+});
